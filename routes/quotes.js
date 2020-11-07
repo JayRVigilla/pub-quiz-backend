4152f85ff39805bad478214ddd593bb3b516a6bf
Jay Vigilla <JayRVigilla@gmail.com>
2020-11-06 17:18:01 -0800

    notes in quotes routes

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,11 +1,20 @@
   /**
    *
    * Quote API
-   *
-   * https://rapidapi.com/collection/quote-generator-apis
+   *  not free, will need to find another one
+   * https://quotes.rest/
    *
    *
    *
    */
 
-   
+  const axios = require('axios');
+  const express = require('express');
+  const ExpressError = require('../helpers/ExpressError');
+
+  const router = new express.Router();
+
+  const BASE_URL = "https://quotes.rest/"
+  // GET random quote  https://quotes.rest/qod?language=en
+  // GET quote categories https://quotes.rest/qod/categories?language=en&detailed=false
+  // GET qutoes from a category
